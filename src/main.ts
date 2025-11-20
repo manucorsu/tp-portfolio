fe5abfc95d42b033d5b5506080a528105660fe91
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-11-20 17:20:20 -0300

    empezar m2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+// Fonts
 import '@fontsource/aldrich';
+import '@fontsource/inter';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
